0b7e55a5b180ce1d196a7b7e209a8cccb14e3abe
Egor <egorbadauch@mail.ru>
2023-10-01 23:14:14 +0500

    Added functionality

---
+++ js/functions.js
@@ -0,0 +1,33 @@
+const checkStringLength = (inputString, maxLength) => inputString.length <= maxLength;
+
+const isPalindrome = input => {
+  const formattedString = input.replaceAll(' ', '').toLowerCase();
+  const reversedString = formattedString.split('').reverse().join('');
+  return formattedString === reversedString;
+}
+
+const highlightNumber = input => {
+  let resultNumber = '';
+  for (let elem of String(input)){
+    const parsedElem = parseInt(elem);
+    resultNumber += (!Number.isNaN(parsedElem)) ? parsedElem : '';
+  }
+  return resultNumber ? +resultNumber : NaN;
+}
+
+checkStringLength('проверяемая строка', 20); //true
+checkStringLength('проверяемая строка', 10); //false
+
+isPalindrome('топот'); //true
+isPalindrome('Кекс'); //false
+
+highlightNumber('2023 год'); //2023
+highlightNumber('1 кефир, 0.5 батона'); //105
+highlightNumber('а я томат'); //NaN
+highlightNumber(1.5); //15
+
+
+
+
+
+
